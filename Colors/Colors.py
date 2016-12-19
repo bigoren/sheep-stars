22b5216b0536a4aa0f7ed91bf0b3dd521c691968
May Ben Arie <may.benarie@vonage.com>
2016-12-20 00:36:50 +0200

    colors - calculate similar colors

---
+++ Colors/Colors.py
@@ -0,0 +1,10 @@
+import colorsys
+
+def adjacent_color((r, g, b)): # Assumption: r, g, b in [0, 255]
+    d = 30/360.0
+    r, g, b = map(lambda x: x/255., [r, g, b]) # Convert to [0, 1]
+    h, l, s = colorsys.rgb_to_hls(r, g, b)     # RGB -> HLS
+    h = [(h+d) % 1 for d in (-d, d)]           # Rotation by d
+    adjacent = [map(lambda x: int(round(x*255)), colorsys.hls_to_rgb(hi, l, s))
+                for hi in h] # H'LS -> new RGB
+    return adjacent
