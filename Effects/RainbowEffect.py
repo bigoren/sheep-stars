ba120dc0e924ef126f0f958c46b5f731296ff13b
May Ben Arie <may.benarie@vonage.com>
2016-12-23 21:26:35 +0200

    rainbow animation

---
+++ Effects/RainbowEffect.py
@@ -0,0 +1,17 @@
+from AbstractEffect import Effect
+
+import colorsys
+import math
+
+class RainbowEffect(Effect):
+    
+    def __init__(self, indexes):
+        Effect.__init__(self, indexes)
+   
+    def apply(self, time_precent, parent_array):
+
+        for i in range(len(self.indexes)):
+            hue = i / float(len(self.indexes)) + time_precent
+            if (hue >= 1):
+                hue -= 1
+            parent_array[self.indexes[i]*3 : self.indexes[i]*3+3] = [int(c*255) for c in colorsys.hsv_to_rgb(hue, 1.0, 0.75)]
