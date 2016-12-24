e8ed39d7fe2634b1434152e1ad55cca8af81a877
bigoren <bigoreo@gmail.com>
2016-12-24 17:54:39 +0200

    snake

--- Effects/FadingSnakeEffect.py
+++ Effects/FadingSnakeEffect.py
@@ -2,33 +2,37 @@ from AbstractEffect import Effect
 
 import colorsys
 import random
+import math
 
 class FadingSnakeEffect(Effect):
-    def __init__(self, indexes, leds_per_cycle):
+    def __init__(self, indexes, number_of_snakes):
         Effect.__init__(self, indexes)
-        self._leds_per_cycle = leds_per_cycle
+        self._number_of_snakes = number_of_snakes
         self.hue_start = random.random()
-        if len(self.indexes) != 0:
-            self.start_pos = random.randint(0, len(self.indexes) - 1)
+        self.start_pos = 0
     
     def apply(self, time_precent, parent_array):
         if len(self.indexes) == 0:
             return
+            
+        curr_snake_pos = int(time_precent * len(self.indexes))
+        print curr_snake_pos
         
-        random_hue = self.hue_start + (random.random() * 0.3) - 0.3/2
-        while random_hue > 1.0:
-            random_hue = random_hue - 1.0
-        while random_hue < 0.0:
-            random_hue = random_hue + 1.0
+        self.hue_start = self.hue_start + 0.01
+        while self.hue_start > 1.0:
+            self.hue_start = self.hue_start - 1.0
+        while self.hue_start < 0.0:
+            self.hue_start = self.hue_start + 1.0
         
         """ fade to black """
         for i in self.indexes:
-            parent_array[3*i] = int(parent_array[3*i] * 0.9)
+            parent_array[3*i] = int(parent_array[3*i] * 0.87)
             parent_array[3*i+1] = int(parent_array[3*i+1] * 0.9)
             parent_array[3*i+2] = int(parent_array[3*i+2] * 0.9)
         
         """ add new full light color pixels """
-        for _ in range(0, 1):
-            pix_index = self.start_pos * 3
-            parent_array[pix_index : pix_index+3] = [int(c*255) for c in colorsys.hsv_to_rgb(random_hue, 0.75, 1.0)]
-            self.start_pos = (self.start_pos + 1) % len(self.indexes)
+        for i in range(0, self._number_of_snakes):
+            pix = self.indexes[curr_snake_pos % len(self.indexes)]
+            parent_array[pix * 3 : (pix + 1) * 3] = [int(c*255) for c in colorsys.hsv_to_rgb(self.hue_start, 0.8, 1.0)]
+            
+        self.start_pos = (self.start_pos + 1) % len(self.indexes)
--- simple_use_blink_animation.py
+++ simple_use_blink_animation.py
@@ -5,7 +5,7 @@ from Song.DreamOn import DreamOn
 from Song.TaPaDa import TaPaDa
 from Song.Borealis import Borealis
 
-song = DreamOn()
+song = FroggyWoogie()
 song.play()
 
 	
